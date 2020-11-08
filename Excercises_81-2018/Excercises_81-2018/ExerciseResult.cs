91ca794d3734ee7514f3f0e7aa9970559ec19cbf
DESKTOP-30N4GA5\Djuro <djurovic.milos1998@gmail.com>
2020-11-08 23:35:24 +0100

    Napravljena klasa ExerciseResult,

---
+++ Excercises_81-2018/Excercises_81-2018/ExerciseResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excercises_81_2018
+{
+    class ExerciseResult
+    {
+        private static int idd = 0;
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Point { get; set; }
+
+        public ExerciseResult(string name, string index, int point)
+        {
+            Id = idd++;
+            StudentName = name;
+            StudentIndex = index;
+            Point = point;
+        }
+        public ExerciseResult() { }
+        public override string ToString()
+       => Id + " " + StudentName + " " + StudentIndex + " " + Point;
+    }
+}
